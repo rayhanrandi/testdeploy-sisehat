30819e5199c7c6e680d33581ae53aefe8f80a222
bryan-ilman <aksesleluasa@gmail.com>
2022-10-13 11:06:22 +0700

    halaman_utama app created

--- SiSehat/settings.py
+++ SiSehat/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'halaman_utama',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'SiSehat.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ halaman_utama/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HalamanUtamaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'halaman_utama'
