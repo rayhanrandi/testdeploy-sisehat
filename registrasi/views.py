882bedaea9dc3f328d5680fc8e37f7400d0d1c8a
bryan-ilman <aksesleluasa@gmail.com>
2022-10-28 19:47:25 +0700

    membuat form registrasi

--- registrasi/views.py
+++ registrasi/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect, render
 from registrasi.forms import MendaftarDokter, MendaftarPasien
-from registrasi.models import Pasien
+from registrasi.models import Dokter, Pasien
 
 def daftar_pasien(request):
     form_mendaftar_pasien = MendaftarPasien()
@@ -33,4 +33,31 @@ def daftar_pasien(request):
     return render(request, "registrasi.html", context)
 
 def daftar_dokter(request):
-    pass
+    form_mendaftar_dokter = MendaftarDokter()
+
+    if request.method == "POST":
+        user = UserCreationForm(request.POST)
+
+        nomor_induk_kependudukan = request.POST.get("nomor_induk_kependudukan")
+        
+        nama_rumah_sakit = request.POST.get("nama_rumah_sakit")
+
+        angka_16_digit = "^\d{16}$"
+        keabsahan_pengguna = re.search(angka_16_digit, nomor_induk_kependudukan)
+
+        if user.is_valid() and keabsahan_pengguna != None:
+            user = user.save()
+
+            dokter = Dokter.objects.create(
+                user = user,
+                nomor_induk_kependudukan = nomor_induk_kependudukan,
+                nama_rumah_sakit = nama_rumah_sakit
+            )
+
+            dokter.save()
+
+            messages.success(request, 'Akun telah berhasil dibuat!')
+
+    context = {'form_mendaftar_dokter':form_mendaftar_dokter}
+
+    return render(request, "registrasi.html", context)
