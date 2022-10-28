e721d4c73f09f5754f119d81175b50395e3b90b0
bryan-ilman <aksesleluasa@gmail.com>
2022-10-28 19:25:30 +0700

    membuat form registrasi

--- registrasi/forms.py
+++ registrasi/forms.py
@@ -1,55 +1,29 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.core.validators import MinValueValidator, MaxValueValidator
-from django.db import models, transaction
 
-from .models import Dokter, Pasien
 
-# jangkauan nomor_induk_kependudukan
-nomor_terkecil = 2_000_000_000_000_000; nomor_terbesar = 10_000_000_000_000_000
+class MendaftarPasien(UserCreationForm):
+    nomor_induk_kependudukan = forms.CharField(min_length=16, max_length=16)
 
-class MendaftarPasien():
-    nomor_induk_kependudukan = models.BigIntegerField(validators=[MinValueValidator(nomor_terkecil),MaxValueValidator(nomor_terbesar)], primary_key=True)
 
     class Meta(UserCreationForm.Meta):
         model = User
-        nomor_induk_kependudukan = models.BigIntegerField(validators=[MinValueValidator(nomor_terkecil),MaxValueValidator(nomor_terbesar)], primary_key=True)
 
-    @transaction.atomic
-    def save(self):
-        user = super().save(commit=False)
-        user.save()
+        membuat_kata_sandi = "password" + "1"
+        memastikan_kata_sandi = "password" + "2"
 
-        nomor_induk_kependudukan = self.cleaned_data.get("nomor_induk_kependudukan")
-        
-        pasien = Pasien.objects.create(
-            user=user,
-            nomor_induk_kependudukan=nomor_induk_kependudukan,
-        )
-        pasien.save()
-
-        return user
+        fields = ("username", "nomor_induk_kependudukan", membuat_kata_sandi, memastikan_kata_sandi)
 
 class MendaftarDokter(UserCreationForm):
-    nomor_induk_kependudukan = models.BigIntegerField(validators=[MinValueValidator(nomor_terkecil),MaxValueValidator(nomor_terbesar)], primary_key=True)
-    nama_rumah_sakit = models.CharField(max_length=128)
+    nomor_induk_kependudukan = forms.CharField(min_length=16, max_length=16)
+    nama_rumah_sakit = forms.CharField(max_length=128)
+
 
     class Meta(UserCreationForm.Meta):
         model = User
 
-    @transaction.atomic
-    def save(self):
-        user = super().save(commit=False)
-        user.save()
+        membuat_kata_sandi = "password" + "1"
+        memastikan_kata_sandi = "password" + "2"
 
-        nomor_induk_kependudukan = self.cleaned_data.get("nomor_induk_kependudukan")
-        nama_rumah_sakit = self.cleaned_data.get("nama_rumah_sakit")
-
-        dokter = Dokter.objects.create(
-            user=user,
-            nomor_induk_kependudukan=nomor_induk_kependudukan,
-            nama_rumah_sakit=nama_rumah_sakit
-        )
-        dokter.save()
-
-        return user
+        fields = ("username", "nomor_induk_kependudukan", "nama_rumah_sakit", membuat_kata_sandi, memastikan_kata_sandi)
--- registrasi/models.py
+++ registrasi/models.py
@@ -1,20 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.validators import MinValueValidator, MaxValueValidator
 
-# jangkauan nomor_induk_kependudukan
-nomor_terkecil = 2_000_000_000_000_000; nomor_terbesar = 10_000_000_000_000_000
 
 class Pasien(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    nomor_induk_kependudukan = models.BigIntegerField(validators=[MinValueValidator(nomor_terkecil),MaxValueValidator(nomor_terbesar)], primary_key=True)
+    nomor_induk_kependudukan = models.CharField(max_length=16, primary_key=True)
 
     status_pasien = True
     status_dokter = False
 
+
 class Dokter(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    nomor_induk_kependudukan = models.BigIntegerField(validators=[MinValueValidator(nomor_terkecil),MaxValueValidator(nomor_terbesar)], primary_key=True)
+    nomor_induk_kependudukan = models.CharField(max_length=16, primary_key=True)
     nama_rumah_sakit = models.CharField(max_length=128)
 
     status_pasien = False
